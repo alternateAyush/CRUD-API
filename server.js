a1a75a3630d7aa9ee8bf470a56366fe10d4d7c66
alternateAyush <ayushbhatt2002@gmail.com>
2023-09-21 00:20:34 +0530

    connected to MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 // routes
@@ -6,6 +7,20 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
-  console.log("app is running on port 3000");
+app.get("/blog", (req, res) => {
+  res.send("Hello Blog");
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:012345Admin@crudapi.zqugxvj.mongodb.net/Crud-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Successfuly connected to mongoDb");
+    app.listen(3000, () => {
+      console.log("app is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.log("Failed to connect to mongoDb: ", error);
+  });
